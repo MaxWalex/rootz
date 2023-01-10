19e85f080e5bc396aef8371af9f7d9ddaec47a7d
Maxim Chervonoshtan <chervonoshtan02@gmail.com>
2023-01-10 19:17:15 +0200

    added section carousel #4

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -52,11 +52,9 @@ function Carousel() {
   return (
     <section className='carousel'>
         <Swiper 
-            slidesPerView={5.5}
-            createElements={true}
+            slidesPerView={4.5}
             centeredSlides={true}
             loop={true}
-            // spaceBetween={52} 
             navigation={{
                 prevEl: navigationPrevRef.current,
                 nextEl: navigationNextRef.current,
@@ -65,13 +63,19 @@ function Carousel() {
                 swiper.params.navigation.prevEl = navigationPrevRef.current;
                 swiper.params.navigation.nextEl = navigationNextRef.current;
             }}
-            pagination={{ clickable: true, type: "fraction" }}
+            pagination={{
+                el: '.custom-pagination',
+                clickable: true,
+                type: "fraction"
+            }}
             modules={[Navigation, Pagination]}
         >
             {carouselData.map(({name, text, img, id}) => {
                 return <SwiperSlide 
                             key={id}>
+                            <div className='swiper_slide-wrapper'>
                             <div className='swiper_slide-img'>
+                                <h3 className='main_title'>{name}</h3>
                                 <img src={img} />
                             </div>
                             <div className='swiper_slide-desc'>
@@ -81,19 +85,26 @@ function Carousel() {
                                 </div>
                                 <img src={slideBg} />
                             </div>
+                            </div>
+                            
                         </SwiperSlide>
             })}
 
-            <div ref={navigationPrevRef}>
-                <svg width="8" height="16" viewBox="0 0 8 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0.212403 7.46465L6.74633 0.721429C7.03304 0.42619 7.49754 0.42619 7.78497 0.721429C8.07168 1.01667 8.07168 1.49621 7.78497 1.79145L1.76931 7.99963L7.78425 14.2078C8.07095 14.503 8.07095 14.9826 7.78425 15.2786C7.49754 15.5738 7.03231 15.5738 6.74561 15.2786L0.211679 8.53535C-0.0706816 8.24317 -0.0706816 7.75609 0.212403 7.46465Z" fill="#1E1A20"/>
-                </svg>
+
+            <div className='swiper_custom-arrows'>
+                <div ref={navigationPrevRef} className='swiper_custom-arrow_left'>
+                    <svg width="8" height="16" viewBox="0 0 8 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0.212403 7.46465L6.74633 0.721429C7.03304 0.42619 7.49754 0.42619 7.78497 0.721429C8.07168 1.01667 8.07168 1.49621 7.78497 1.79145L1.76931 7.99963L7.78425 14.2078C8.07095 14.503 8.07095 14.9826 7.78425 15.2786C7.49754 15.5738 7.03231 15.5738 6.74561 15.2786L0.211679 8.53535C-0.0706816 8.24317 -0.0706816 7.75609 0.212403 7.46465Z" fill="#1E1A20"/>
+                    </svg>
+                </div>
+                <div className='custom-pagination'></div>
+                <div ref={navigationNextRef} className='swiper_custom-arrow_right'>
+                    <svg width="8" height="16" viewBox="0 0 8 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M7.7876 7.46465L1.25367 0.721429C0.966964 0.42619 0.502457 0.42619 0.215028 0.721429C-0.0716761 1.01667 -0.0716761 1.49621 0.215028 1.79145L6.23069 7.99963L0.215752 14.2078C-0.070952 14.503 -0.070952 14.9826 0.215752 15.2786C0.502457 15.5738 0.967688 15.5738 1.25439 15.2786L7.78832 8.53535C8.07068 8.24317 8.07068 7.75609 7.7876 7.46465Z" fill="#1E1A20"/>
+                    </svg>
+                </div>
             </div>
-            <div ref={navigationNextRef}>
-                <svg width="8" height="16" viewBox="0 0 8 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M7.7876 7.46465L1.25367 0.721429C0.966964 0.42619 0.502457 0.42619 0.215028 0.721429C-0.0716761 1.01667 -0.0716761 1.49621 0.215028 1.79145L6.23069 7.99963L0.215752 14.2078C-0.070952 14.503 -0.070952 14.9826 0.215752 15.2786C0.502457 15.5738 0.967688 15.5738 1.25439 15.2786L7.78832 8.53535C8.07068 8.24317 8.07068 7.75609 7.7876 7.46465Z" fill="#1E1A20"/>
-                </svg>
-            </div>
+            
         </Swiper>
     </section>
   )
