469c5b20084cdfa35f4514f682cc50d19a43fb50
Maxim Chervonoshtan <chervonoshtan02@gmail.com>
2023-01-11 00:22:03 +0200

    parallax

--- src/components/logIn/LogIn.jsx
+++ src/components/logIn/LogIn.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import leave1 from '../../images/leave1.svg'
 import leave4 from '../../images/leave4.svg'
@@ -10,6 +10,8 @@ function LogIn() {
     const [emailText, setEmailText] = useState('')
     const [dangerTextName, setDangerTextName] = useState('')
     const [dangerTextEmail, setDangerTextEmail] = useState('')
+
+    const logInRef = useRef()
 
     const handleSubmit = e => {
         e.preventDefault()
@@ -29,10 +31,28 @@ function LogIn() {
         setNameText('')
         setEmailText('')
     }
+
+    const handleOnMouseMove = e => {
+        const loginForm = logInRef.current  
+
+        const parallax = (data) => {
+            data.forEach(layer => {
+                const speed = layer.attributes[1].value;
+    
+                const x = (window.innerWidth - e.pageX*speed)/100
+                const y = (window.innerHeight - e.pageY*speed)/100
+    
+                layer.style.transform = `translate(${x}px, ${y}px)`
+            })
+        }
+
+        parallax(Array.from(loginForm.querySelectorAll('.login_right-leaves img')))
+    }
+
   return (
     <section className='login'>
         <div className="container">
-            <div className="login_inner">
+            <div className="login_inner" ref={logInRef} onMouseMove={e => handleOnMouseMove(e)}>
                 <div className="login_left">
                     <h2 className='main_title'>Get started today!</h2>
                     <p>Learn more about how you can save our planet's nature. From smart consumption to switching to renewable energy, each of us can do our part to save the planet. </p>
@@ -49,8 +69,8 @@ function LogIn() {
 
                         <button className='main_button'>Book a Demo</button>
                         <div className='login_right-leaves'>
-                            <img src={leave1} />
-                            <img src={leave4} />
+                            <img src={leave1} data-speed="1" />
+                            <img src={leave4} data-speed="-1" />
                         </div>
                     </form>
                 </div>
